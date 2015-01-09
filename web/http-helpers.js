ec3b795ff6ec69049f25c7a0a353b17860cb046a
Adam Back <adb33@email.arizona.edu>
2015-01-08 16:18:45 -0800

    First test passing.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var Q = require('q');
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
@@ -10,9 +11,20 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
-exports.serveAssets = function(res, asset, callback) {
+exports.serveAssets = function(asset) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  var deferred = Q.defer();
+
+  fs.readFile(archive.paths.siteAssets + '/' + asset, 'utf-8', function(error, assetFile) {
+    if( error ) {
+      deferred.reject(error);
+    } else {
+      deferred.resolve(assetFile);
+    }
+  });
+
+  return deferred.promise;
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,31 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if( req.method === 'GET' ) {
+    if ( req.url === '/' ) {
+      // serve static index.html
+      httpHelpers.serveAssets('index.html')
+        .then(function(asset) {
+          res.writeHead(200, 'text/html');
+          res.end(asset);
+        })
+        .catch(function(error) {
+          console.log("Error serving index.html:", error);
+        });
+    } else if ( req.url === '/styles.css' ) {
+      httpHelpers.serveAssets( 'styles.css' ) 
+        .then(function(stylesheet) {
+          res.writeHead(200, 'text/css');
+          res.end(stylesheet);
+        })
+        .catch(function(error) {
+          console.log("Error serving css", error);
+        });
+    }
+  } else if( req.method === 'POST' ) {
+
+  }
 };
